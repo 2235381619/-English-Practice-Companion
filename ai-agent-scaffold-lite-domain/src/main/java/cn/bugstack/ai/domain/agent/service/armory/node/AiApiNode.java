46ba959b0e2ef66f587c300d04b9bbf84329d580
fuzhengwei <184172133@qq.com>
2025-12-23 08:21:47 +0800

    feat: 第2-5节：装配域节点-AiApiNode

--- ai-agent-scaffold-lite-domain/src/main/java/cn/bugstack/ai/domain/agent/service/armory/node/AiApiNode.java
+++ ai-agent-scaffold-lite-domain/src/main/java/cn/bugstack/ai/domain/agent/service/armory/node/AiApiNode.java
@@ -1,11 +1,14 @@
 package cn.bugstack.ai.domain.agent.service.armory.node;
 
 import cn.bugstack.ai.domain.agent.model.entity.ArmoryCommandEntity;
+import cn.bugstack.ai.domain.agent.model.valobj.AiAgentConfigTableVO;
 import cn.bugstack.ai.domain.agent.model.valobj.AiAgentRegisterVO;
 import cn.bugstack.ai.domain.agent.service.armory.AbstractArmorySupport;
 import cn.bugstack.ai.domain.agent.service.armory.factory.DefaultArmoryFactory;
 import cn.bugstack.wrench.design.framework.tree.StrategyHandler;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -14,9 +17,20 @@ public class AiApiNode extends AbstractArmorySupport {
 
     @Override
     protected AiAgentRegisterVO doApply(ArmoryCommandEntity requestParameter, DefaultArmoryFactory.DynamicContext dynamicContext) throws Exception {
-        // 编写 AiApi 构建
+        log.info("Ai Agent 装配操作 - AiApiNode");
 
-        // 路由到下一个节点，如果不需要路由了，可以 return 返回结果
+        AiAgentConfigTableVO aiAgentConfigTableVO = requestParameter.getAiAgentConfigTableVO();
+        AiAgentConfigTableVO.Module.AiApi aiApiConfig = aiAgentConfigTableVO.getModule().getAiApi();
+
+        OpenAiApi openAiApi = OpenAiApi.builder()
+                .baseUrl(aiApiConfig.getBaseUrl())
+                .apiKey(aiApiConfig.getApiKey())
+                .completionsPath(StringUtils.isNotBlank(aiApiConfig.getCompletionsPath()) ? aiApiConfig.getCompletionsPath() : "v1/chat/completions")
+                .embeddingsPath(StringUtils.isNotBlank(aiApiConfig.getEmbeddingsPath()) ? aiApiConfig.getEmbeddingsPath() : "v1/embeddings")
+                .build();
+
+        dynamicContext.setOpenAiApi(openAiApi);
+
         return router(requestParameter, dynamicContext);
     }
 
